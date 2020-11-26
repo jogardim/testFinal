e75864da1bc0bbbbaf45dcb1a2dffee193836c41
Gardim <jbgardim@gmail.com>
2020-11-26 15:37:03 -0300

    test final

--- src/test/TestFinal.java
+++ src/test/TestFinal.java
@@ -23,11 +23,11 @@ public class TestFinal {
     }
 
     public static void main(String... args) {
-        Instant initFinal = Instant.now();
+        final Instant initFinal = Instant.now();
         for (int i = 0; i < ITERATIONS; i++) {
             testFinal();
         }
-        Instant endFinal = Instant.now();
+        final Instant endFinal = Instant.now();
         System.out.println("With final: " + Duration.between(initFinal, endFinal).toMillis());
 
         Instant initNonFinal = Instant.now();
